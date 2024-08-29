8a076e3fc33ceb4209f76dd830057d61835df71f
Velizar <31567122+VelizarBG@users.noreply.github.com>
2024-08-29 21:37:16 +0300

    Fix remapping error and make suppressionCount work outside dev env

--- src/main/java/me/wavetech/wavecarpet/mixins/feats/suppressionCount/PacketUtilsMixin.java
+++ src/main/java/me/wavetech/wavecarpet/mixins/feats/suppressionCount/PacketUtilsMixin.java
@@ -1,23 +1,26 @@
 package me.wavetech.wavecarpet.mixins.feats.suppressionCount;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import me.wavetech.wavecarpet.core.ObjectiveCriteriaRegistry;
+import net.minecraft.ReportedException;
 import net.minecraft.network.PacketListener;
 import net.minecraft.network.protocol.PacketUtils;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.scores.ScoreAccess;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
 
 @Mixin(PacketUtils.class)
 public class PacketUtilsMixin {
-	@Inject(method = "lambda$ensureRunningOnSameThread$0", at = @At(value = "CONSTANT", args = "classValue=net/minecraft/ReportedException", ordinal = 0))
-	private static void countSuppression(CallbackInfo ci, @Local(argsOnly = true) PacketListener packetListener) {
+	// TODO: Incinerate this crap when MixinExtras expressions drop in Loader
+	@WrapOperation(method = "lambda$ensureRunningOnSameThread$0", constant = @Constant(classValue = ReportedException.class, ordinal = 0))
+	private static boolean countSuppression(Object object, Operation<Boolean> original, @Local(argsOnly = true) PacketListener packetListener) {
 		if (packetListener instanceof ServerGamePacketListenerImpl gamePL) {
 			gamePL.player.server.getScoreboard()
 				.forAllObjectives(ObjectiveCriteriaRegistry.SUPPRESSION_COUNT, gamePL.player, ScoreAccess::increment);
 		}
+		return original.call(object);
 	}
 }
